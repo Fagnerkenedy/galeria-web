d173fb5a6f580215c91416ad61664d94e0abeae1
Laercio Guarda <laercio@wikiconsultoria.com.br>
2022-07-06 17:57:22 -0300

    Alterações

--- src/App.js
+++ src/App.js
@@ -4,20 +4,19 @@ import './App.css';
 
 import Home from "./Home"
 import Cadastro from "./component/users/Cadastro"
-import Layout from "./component/layout"
+import Login from "./component/users/Login"
 import PageNotFound from "./PageNotFound"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/cadastro" element={<Cadastro />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </Layout>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/cadastro" element={<Cadastro />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/component/users/Cadastro.jsx
+++ src/component/users/Cadastro.jsx
@@ -1,18 +1,91 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input, Row, Col, Layout } from 'antd';
+import { Layout, Row, Col, Button, Checkbox, Form, Input, Typography, Divider } from 'antd';
 import React from 'react';
 
+import logo from '../../img/logo.jpg';
+
 const { Content } = Layout;
-const DemoBox = (props) => <p className={`height-${props.value}`}>{props.children}</p>;
+const { Title } = Typography;
 
-const Cadastro = () => (
 
-    <Row justify="space-around" align="middle">
-        <Col span={4}>
-            <DemoBox value={100}>col-4</DemoBox>
-        </Col>
-    </Row>
 
-);
+const Cadastro =  () => {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+      };
+      return (
+        <Layout className="layout">
+            <Content>
+                <div className='user-row-cadastro'>
+                <Row>
+                    <Col span={6} offset={9}>
+                        <div className='user-content-cadastro'>
+                            <Row>
+                                <Col span={20} offset={2}>
+                                    <img className='user-cadastro-logo' src={logo} />
+                                    <Title level={3} className='user-cadastro-title'>Faça seu cadastro</Title>
+                                    <Divider />
+                                    <Form
+                                        name="normal_login"
+                                        className="login-form"
+                                        initialValues={{
+                                            remember: true,
+                                        }}
+                                        onFinish={onFinish}
+                                    >
+                                        <Form.Item
+                                            name="username"
+                                            rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your Username!',
+                                            },
+                                            ]}
+                                        >
+                                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                                        </Form.Item>
+                                        <Form.Item
+                                            name="password"
+                                            rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your Password!',
+                                            },
+                                            ]}
+                                        >
+                                            <Input
+                                            prefix={<LockOutlined className="site-form-item-icon" />}
+                                            type="password"
+                                            placeholder="Password"
+                                            />
+                                        </Form.Item>
+                                        <Form.Item>
+                                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                            <Checkbox>Remember me</Checkbox>
+                                            </Form.Item>
+
+                                            <a className="login-form-forgot" href="">
+                                            Forgot password
+                                            </a>
+                                        </Form.Item>
+
+                                        <Form.Item>
+                                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                            Log in
+                                            </Button>
+                                            Or <a href="">register now!</a>
+                                        </Form.Item>
+                                    </Form>
+
+                                </Col>
+                            </Row>
+                        </div>
+                    </Col>
+                </Row>
+                </div>
+            </Content>
+        </Layout>
+    )
+};
 
 export default Cadastro;
