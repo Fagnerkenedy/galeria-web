4982374872af181bf3e435ad7018d054ddcdd156
Laercio Guarda <laercio@wikiconsultoria.com.br>
2022-07-06 21:50:35 -0300

    Novas Alterações

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ConfigProvider } from 'antd'
+import ptBR from 'antd/lib/locale/pt_BR'
 import 'antd/dist/antd.min.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={ptBR}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
